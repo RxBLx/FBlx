fb29ab32d6d967c32046558326e27b94a1f94682
RxBLx <babussalamprojex@gmail.com>
2025-05-15 05:01:56 +0700

    Update main-scripts.js

--- main-scripts.js
+++ main-scripts.js
@@ -1,4 +1,4 @@
-// script.js
+// Responsive ads resizing for mobile
 function resizeAds() {
   const ads = document.querySelectorAll('section > div[style], main > div[style], section[style] > div[style]');
   ads.forEach(ad => {
@@ -11,10 +11,10 @@ function resizeAds() {
     }
   });
 }
-
 window.addEventListener('resize', resizeAds);
 window.addEventListener('load', resizeAds);
 
+// Subscribe modal logic
 const modal = document.getElementById('subscribe-modal');
 const closeBtn = document.getElementById('close-subscribe');
 
@@ -32,17 +32,49 @@ closeBtn.addEventListener('click', () => {
   localStorage.setItem('rxblSubscribeDismissed', 'true');
 });
 
+// Close modal on Escape key
 window.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && modal.classList.contains('active')) {
     closeBtn.click();
   }
 });
 
+// Show modal every 15 minutes if not dismissed
 window.addEventListener('load', () => {
   showSubscribeModal();
   setInterval(() => {
     if (!localStorage.getItem('rxblSubscribeDismissed')) {
       showSubscribeModal();
     }
-  }, 900000); // 15 menit
+  }, 900000); // 900,000 ms = 15 minutes
+});
+
+// Inject 728x90 ads into placeholders
+window.addEventListener('load', () => {
+  const adTop = document.getElementById('ad728x90-top');
+  const adMiddle = document.getElementById('ad728x90-middle');
+  const adBottom = document.getElementById('ad728x90-bottom');
+  const adMiddle2 = document.getElementById('ad728x90-middle2');
+  const adBottom2 = document.getElementById('ad728x90-bottom2');
+  if (adTop)
+    adTop.innerHTML = '<iframe src="//www.highperformanceformat.com/7e148f8ba72aa3ad7780fc5c68089875/invoke.js" style="width:728px; height:90px; border:none; overflow:hidden;" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" allowtransparency="true"></iframe>';
+  if (adMiddle)
+    adMiddle.innerHTML = '<iframe src="//www.highperformanceformat.com/7e148f8ba72aa3ad7780fc5c68089875/invoke.js" style="width:728px; height:90px; border:none; overflow:hidden;" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" allowtransparency="true"></iframe>';
+  if (adBottom)
+    adBottom.innerHTML = '<iframe src="//www.highperformanceformat.com/7e148f8ba72aa3ad7780fc5c68089875/invoke.js" style="width:728px; height:90px; border:none; overflow:hidden;" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" allowtransparency="true"></iframe>';
+  if (adMiddle2)
+    adMiddle2.innerHTML = '<iframe src="//www.highperformanceformat.com/7e148f8ba72aa3ad7780fc5c68089875/invoke.js" style="width:728px; height:90px; border:none; overflow:hidden;" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" allowtransparency="true"></iframe>';
+  if (adBottom2)
+    adBottom2.innerHTML = '<iframe src="//www.highperformanceformat.com/7e148f8ba72aa3ad7780fc5c68089875/invoke.js" style="width:728px; height:90px; border:none; overflow:hidden;" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" allowtransparency="true"></iframe>';
+});
+
+// Inject 320x100 mobile ad
+window.addEventListener('load', () => {
+  const ad320 = document.getElementById('ad320x100');
+  if (ad320) {
+    ad320.innerHTML = `<div id="frame" style="width: 320px;">
+      <iframe data-aa='2394475' src='//ad.a-ads.com/2394475?size=320x100' style='width:320px; height:100px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+      <a style="display: block; text-align: right; font-size: 12px" id="frame-link" href="https://aads.com/campaigns/new/?source_id=2394475&source_type=ad_unit&partner=2394475 ">Advertise here</a>
+    </div>`;
+  }
 });
